e43dceaa23008bf2261af4328dc48a1fc6648f37
Oscar Alba <oscaralba33@gmail.com>
2018-07-24 11:44:02 -0500

    Listo

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
-const server = http.createServer(app);
 
+console.log('Server Started...1');
 app.use(express.static(__dirname + "/dist/"));
-
+console.log('Server Started...2');
 app.get(/.*/, function(req, res){
   res.sendfile(__dirname +"/dist/index.html");
 });
 
 app.listen(port);
 
-console.log('Server Started...');
+console.log('Server Started...3');
